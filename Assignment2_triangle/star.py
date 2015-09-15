c883b9ee2516a7701c0159df2ec8e9368b0d3746
nibud <daan_donn@hotmail.com>
2015-09-15 23:52:55 +0200

    pyramid and star added

--- Assignment2_triangle/star.py
+++ Assignment2_triangle/star.py
@@ -2,8 +2,40 @@ __author__ = 'Mac'
 
 number = input('type a number ')
 
+print "[+] pyramid \n"
+
+for i in range(1, number):
+    pyra = ""
+    for j in range(number - i):
+        pyra += "    "
+    for j in range(1, i):
+        pyra += "*   "
+    for j in range(i, 0, -1):
+        pyra += "*   "
+
+    print pyra
+
+print "\n [+] star \n"
+
+
+for i in range(1, number):
+    star = ""
+    for j in range(number - i):
+        star += "    "
+    for j in range(1, i):
+        star += "*   "
+    for j in range(i, 0, -1):
+        star += "*   "
+    print star
+
 for i in range(0, number):
-    if i != number - 1:
-        print '*' * ((number-1)-i) + ' ' * i + '*'
-    else:
-        print '*' * number
+    star = ""
+    for j in range(0, i):
+        star += "    "
+    for j in range(0, number - i - 1):
+        star += "*   "
+    for j in range(number, i, -1):
+        star += "*   "
+
+
+    print star
