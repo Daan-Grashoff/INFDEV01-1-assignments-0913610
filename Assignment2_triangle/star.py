4528736ada34868d624a2c5055906f19fee1e00e
nibud <daan_donn@hotmail.com>
2015-09-16 00:11:15 +0200

    custom char as input

--- Assignment2_triangle/star.py
+++ Assignment2_triangle/star.py
@@ -1,6 +1,20 @@
 __author__ = 'Mac'
 
-number = input('type a number ')
+
+
+try:
+    number = input('type a number ')
+except SyntaxError:
+    number = 5
+
+try:
+    star_char = raw_input('star as ')
+except SyntaxError:
+    star_char = '*'
+
+if star_char == '':
+    star_char = "^^^"
+
 
 print "[+] pyramid \n"
 
@@ -24,7 +38,7 @@ for i in range(1, number):
         star += "    "
     for j in range(1, i):
         star += "*   "
-    for j in range(i, 0, -1):
+    for j in range(-i, 0):
         star += "*   "
     print star
 
@@ -34,8 +48,29 @@ for i in range(0, number):
         star += "    "
     for j in range(0, number - i - 1):
         star += "*   "
-    for j in range(number, i, -1):
+    for j in range(0, number - i):
         star += "*   "
+    print star
+
+print "\n [+] Custom star with custom char \n"
 
 
+for i in range(1, number):
+    star = ""
+    for j in range(number - i):
+        star += "  " + (' ' * int(len(star_char)-1))
+    for j in range(1, i):
+        star += "%s " % star_char
+    for j in range(-i, 0):
+        star += "%s " % star_char
+    print star
+
+for i in range(0, number):
+    star = ""
+    for j in range(0, i):
+        star += "  " + (' ' * int(len(star_char)-1))
+    for j in range(0, number - i - 1):
+        star += "%s "  % star_char
+    for j in range(0, number - i):
+        star += "%s " % star_char
     print star
