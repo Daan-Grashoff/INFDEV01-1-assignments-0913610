4a17161db33256146cf53f7b8b307937ef794bdd
nibud <daan_donn@hotmail.com>
2015-09-15 12:51:07 +0200

    Opdracht

--- Assignment1_Greeting/Greeting.py
+++ Assignment1_Greeting/Greeting.py
@@ -1,6 +1,7 @@
 __author__ = 'Mac'
 
-print "Hello %s how are you" % raw_input('what is ur name? ')
+print "Hello %s how are you?" % raw_input('what is ur name? ')
+
 
 # name = raw_input('What is ur name? ')
 # print "Hello %s how are you?" % name
